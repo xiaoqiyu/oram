d7e6ec6e1552142978ddd7c3adf2d562b471487d
xiaoqiyu <rpyxqi@gmail.com>
2015-10-16 16:57:27 +0800

    Initial Commit

---
+++ oram_new/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  oram_new
+//
+//  Created by Xiaoqi Yu on 16/10/15.
+//  Copyright (c) 2015 Xiaoqi Yu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
