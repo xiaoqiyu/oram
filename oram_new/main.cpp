ea39ff4f4012410c7063d1ebbd32198713edfd3a
xiaoqiyu <rpyxqi@gmail.com>
2015-10-21 11:54:20 +0800

    1st stable version

--- oram_new/main.cpp
+++ oram_new/main.cpp
@@ -39,8 +39,7 @@ int main(int argc, const char * argv[]) {
     mymap.erase(1);
     cout<<mymap.size()<<endl;
      */
-    
-    cout<<"helo new ver"<<endl;
+    cout<<"stable version"<<endl;
     testOram();
     //testOram2Tree();
     //testPathOram();
